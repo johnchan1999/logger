6894625051fa3c8e451ddbcbda0296def3962ab6
johnchan1999 <jun2428941453@gmail.com>
2024-10-03 00:07:18 +0800

    [feat|git|add] add file named .gitignore to ignore folder build

--- logger/src/main.cpp
+++ logger/src/main.cpp
@@ -1,8 +1,12 @@
 #include <nlohmann/json.hpp>
 #include <iostream>
+#include <string.h>
+
+
 
 using namespace std;
+using json = nlohmann::json;
 int main(void) {
-  cout<<"this is logger project"<<"\n";
+  cout<<"this is logger project"<<endl;
   return 0;
 }
